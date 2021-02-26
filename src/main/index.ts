dca56b634bfe4c21ccb1937e8933ebb2825a3039
Cannon <teamnapinfo@gmail.com>
2021-02-26 22:38:55 +0000

    Update index.ts

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,5 +1,4 @@
-import client from '../functions/client'
-import { logger as login } from '../functions/client'
+import client, { logger as login } from '../functions/client'
 
 client.on('ready', () => {
     console.log('Active as ' + client.user!.tag)
