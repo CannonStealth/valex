0a1ee4aefefef7b54d68b23ec0c81329495f68db
Cannon <73777697+CannonStealth@users.noreply.github.com>
2021-02-26 16:28:16 +0000

    Update index.ts

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,8 +1,6 @@
 import client from '../functions/client'
 import { logger as login } from '../functions/client'
 
-// WebHook Manager
-
 client.on('ready', () => {
     console.log('Active as ' + client.user!.tag)
 })
