9c4704a49cb020072ebb0af43fe395a48cf26ea2
Cannon <73777697+CannonStealth@users.noreply.github.com>
2021-02-26 16:17:19 +0000

    Testing

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,6 +1,8 @@
 import client from '../functions/client'
 import { logger as login } from '../functions/client'
 
+// testing
+
 client.on('ready', () => {
     console.log('Active as ' + client.user!.tag)
 })
